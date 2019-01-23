8e8ac98e2fad180b5937f7adc360c46e5177c6f9
Arni Magnusson <arnima@hafro.is>
2019-01-23 22:08:38 +0100

    Basic package

---
+++ R/run.R
@@ -0,0 +1,44 @@
+#' Run TAF Analysis
+#'
+#' Run entire TAF analysis, both bootstrap procedure and scripts.
+#'
+#' @param force whether to remove existing directory.
+#'
+#' @note
+#' This function creates a new working directory with the same name as the
+#' package, before running the bootstrap procedure (\code{taf.bootstrap}) and
+#' TAF scripts (\code{sourceAll}).
+#'
+#' @seealso
+#' \code{\link[icesTAF]{taf.bootstrap}} sets up data files and software.
+#'
+#' \code{\link[icesTAF]{sourceAll}} runs all TAF scripts.
+#'
+#' \code{\link[icesTAF]{icesTAF-package}} gives an overview of the icesTAF
+#' package.
+#'
+#' @importFrom icesTAF cp mkdir sourceAll taf.bootstrap
+#' @importFrom utils packageName
+#'
+#' @examples
+#' run(force=TRUE)
+#'
+#' @export
+
+run <- function(force=FALSE)
+{
+  ## Get package name
+  pkg <- packageName()
+
+  ## Create new working directory, copy TAF analysis from package
+  if(dir.exists(pkg) & !force)
+    stop("directory ", pkg, " exists")
+  unlink(pkg, recursive=TRUE)
+  mkdir(pkg)
+  cp(file.path(system.file(package=pkg), "taf/*"), pkg)
+  owd <- setwd(pkg); on.exit(setwd(owd))
+
+  ## Run analysis
+  taf.bootstrap()
+  sourceAll()
+}
---
+++ inst/taf/model.R
@@ -0,0 +1,19 @@
+## Run analysis, write model results
+
+## Before: catch.csv, survey.csv (data)
+## After:  dls.txt (model)
+
+library(icesTAF)
+taf.library()
+library(icesAdvice)
+
+mkdir("model")
+
+## Get catch and survey data
+catch <- read.taf("data/catch.csv")
+survey <- read.taf("data/survey.csv")
+
+## Apply DLS method 3.2
+dls <- DLS3.2(mean(catch$Catch), survey$Index, len=c(5,2))
+
+write.dls(dls, "model/dls.txt")
